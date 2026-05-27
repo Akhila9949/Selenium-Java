2088505fce4c7da60950855c9c26c29c07900bda
Akhila <Akhila@DESKTOP-RC67CDJ>
2026-05-28 00:12:11 +0530

    27-05-2027 Updated Assignment

--- src/seleniumpractice/ActionMethodsDemo.java
+++ src/seleniumpractice/ActionMethodsDemo.java
@@ -7,13 +7,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ActionMethodsDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver=new ChromeDriver();
-		driver.get("https://en.wikipedia.org/");
+		driver.get("https://www.wikipedia.org/");
 		driver.manage().window().maximize();
-		WebElement inputTextBox=driver.findElement(By.className("cdx-text-input__input"));
+		WebElement inputTextBox=driver.findElement(By.xpath("//input[@id=\"searchInput\"]"));
 		inputTextBox.sendKeys("Selenium");
-		
+		WebElement SearchButton=driver.findElement(By.xpath("//button[@type=\"submit\"]"));
+		SearchButton.click();
+		Thread.sleep(3000);
+		driver.quit();
+				
 		
 		
 	}
